a19a477146c487f0bad2d1d655fbc08af0cd39a6
Petexy / Linexy <piotr.petexiness@gmail.com>
2025-09-11 21:13:10 +0200

    250912-1

---
+++ pkg/davinci-installer/usr/share/linexin/widgets/localization/ru_RU/davinci_installer_dictionary.py
@@ -0,0 +1,9 @@
+translations = {
+    "DaVinci Installer": "Установщик DaVinci",
+
+    "DaVinci Resolve Installer": "Установщик DaVinci Resolve",
+    "Install DaVinci Resolve from .run file": "Установить DaVinci Resolve из файла .run",
+    "Supports both DaVinci Resolve and DaVinci Resolve Studio": "Поддерживает DaVinci Resolve и DaVinci Resolve Studio",
+    "Select Installer File": "Выберите файл установщика",
+    "Installation cancelled.": "Установка отменена."
+}
